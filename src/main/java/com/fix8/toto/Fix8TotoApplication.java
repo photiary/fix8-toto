b06a67c76806a2be0cbd0e8d56c7e230f93c8b41
hoon choi <photiary@gmail.com>
2024-01-01 11:46:26 +0900

    init

---
+++ src/main/java/com/fix8/toto/Fix8TotoApplication.java
@@ -0,0 +1,13 @@
+package com.fix8.toto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fix8TotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fix8TotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fix8/toto/Fix8TotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fix8.toto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fix8TotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
